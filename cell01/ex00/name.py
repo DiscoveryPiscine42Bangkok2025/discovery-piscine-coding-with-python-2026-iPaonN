a71352d1f82bd7b262f188563ece767b7882648e
pao <66070112@kmitl.ac.th>
2026-02-09 15:42:41 +0700

    finished cell01.0 and cell01.1

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "panot"
+last_name = "liangpiboon"
+print(f"{first_name} {last_name}\n")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "panot"
+last_name = "liangpiboon"
+whole_name = first_name + " " + last_name
+print(whole_name)
