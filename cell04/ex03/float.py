3f71df12c43c89c293b9693030fc3f1a81918ef9
pao <66070112@kmitl.ac.th>
2026-02-11 22:14:21 +0700

    finally done with cell4.3 and cell4.4

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -2,7 +2,7 @@
 
 in_num = float(input("Give me a number: "))
 
-if str(in_num).isdigit():
+if in_num == int(in_num):
     print("This number is an integer.")
 else:
     print("This number is a decimal.")
